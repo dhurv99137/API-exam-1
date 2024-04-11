726383b148e9a74de6d6d3db0c4f3ff700645aa1
Dhruv Mangukiya <159777363+dhurv99137@users.noreply.github.com>
2024-04-11 14:10:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const uimaker = (data) => {
+    document.getElementById("container").innerHTML = "";
     data.map(product => {
         let title = document.createElement("h5");
         title.innerHTML = product.title;
         let price = document.createElement("h4");
         price.innerHTML = product.price;
-        let category = document.createElement("h5")
+        let category = document.createElement("h5");
         category.innerHTML = product.category;
-        let description = document.createElement("h5");
+        let description = document.createElement("p");
         description.innerHTML = product.description;
         let image = document.createElement("img");
         image.src = product.image;
@@ -17,22 +18,21 @@ const uimaker = (data) => {
     });
 }
 
+let originalData = []; 
 
-let data=[]
 fetch("https://fakestoreapi.com/products")
     .then((response) => response.json())
     .then((datas) => {
-data=datas
-        uimaker(datas)
-    })
-
-
+        originalData = datas;
+        uimaker(datas);
+    });
 
 const filter = (val) => {
-    let temp = data.filter((ele) => ele.category == (val))
-    uimaker(temp)
-}
-document.getElementById("men").addEventListener("click", () => filter("men's clothing"))
-document.getElementById("jewelery").addEventListener("click", () => filter("jewelery"))
-document.getElementById("electronics").addEventListener("click", () => filter("electronics"))
-document.getElementById("women").addEventListener("click", () => filter("women's clothing"))
+    let temp = originalData.filter((ele) => ele.category === val);
+    uimaker(temp);
+};
+
+document.getElementById("men").addEventListener("click", () => filter("men's clothing"));
+document.getElementById("jewelery").addEventListener("click", () => filter("jewelery"));
+document.getElementById("electronics").addEventListener("click", () => filter("electronics"));
+document.getElementById("women").addEventListener("click", () => filter("women's clothing"));
