ba18bd99bc2a6043584ef2030decba1c42d37f31
Dhruv Mangukiya <159777363+dhurv99137@users.noreply.github.com>
2024-04-12 15:06:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const uimaker = (data) => {
     });
 }
 
-let originalData = []; 
+let originalData = [];
 
 fetch("https://fakestoreapi.com/products")
     .then((response) => response.json())
@@ -27,12 +27,38 @@ fetch("https://fakestoreapi.com/products")
         uimaker(datas);
     });
 
+// sorting
+const sortingdata = () => {
+    originalData.sort((a, b) => a.price - b.price);
+    uimaker(originalData);
+}
+const datasorting = () => {
+    originalData.sort((a, b) => b.price - a.price);
+    uimaker(originalData);
+}
+// filter
 const filter = (val) => {
     let temp = originalData.filter((ele) => ele.category === val);
     uimaker(temp);
 };
+// search
+const datasearch = (val) => {
+    let temp = originalData.filter((ele) => ele.title == (val))
+    uimaker(temp)
+}
+const searchdata = (e) => {
+    e.preventDefault()
+    let val = document.getElementById("val").value
+    datasearch(val)
+}
 
+// sorting
+document.getElementById("low").addEventListener("click", sortingdata)
+document.getElementById("high").addEventListener("click", datasorting)
+// filter
 document.getElementById("men").addEventListener("click", () => filter("men's clothing"));
 document.getElementById("jewelery").addEventListener("click", () => filter("jewelery"));
 document.getElementById("electronics").addEventListener("click", () => filter("electronics"));
 document.getElementById("women").addEventListener("click", () => filter("women's clothing"));
+// search
+document.getElementById("search").addEventListener("submit", searchdata)
